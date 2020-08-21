c83a182d7cdac8a8fe3f7fc3106e4be08322cb42
Gandhar Tandale <gandhar.tandale14@gmail.com>
2020-08-21 20:43:03 +0530

    Adds filtering logic based on phone_number

--- api/models.py
+++ api/models.py
@@ -1,5 +1,15 @@
+from datetime import timedelta
+
 from django.db import models
 from django.utils import timezone
+
+
+class SmsManager(models.Manager):
+    def get_queryset(self):
+        # return super().get_quesyset().filter(self.model, using=self._db)
+        # return self.filter(created_date__gte=min_time)
+        min_time = timezone.now() - timedelta(minutes=30)
+        return super().get_queryset().filter(created_at__gte=min_time)
 
 
 # Create your models here.
@@ -9,6 +19,7 @@ class Sms(models.Model):
     sms_body = models.CharField(max_length=500)
     created_by = models.CharField(max_length=20, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    valid_sms = SmsManager()
 
     class Meta:
         ordering = ['-created_at', ]
