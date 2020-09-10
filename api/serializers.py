b9ed5b7cef640c34333927d947906d4313f0303f
Gandhar Tandale <gandhar.tandale14@gmail.com>
2020-09-11 00:14:27 +0530

    Cleanup

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,10 @@
+
 from .models import Sms
 from rest_framework import serializers
 
 
 class SmsSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Sms
-        fields = ['id', 'phone_number', 'sms_body', 'created_at']
+        fields = ['phone_number', 'sms_body', 'created_at']
